f1f047d0d894562043671ec76663e1874412d5d8
Madeline Almeida <codesbymad@gmail.com>
2026-09-01 19:29:11 -0300

    feat: implement product update controllers

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -36,12 +36,38 @@ export const totalProdutos = async (req, res) => {
 
 export const deletarProduto = async(req, res) => {
 
+    
     try{
         const produto = await Produto.destroy({
         where: { id: req.params.id}
     })
 
     res.status(200).json(produto)
+}catch(err){
+    res.status(500).json(err)
+}
+}
+
+export const editarProduto = async(req, res) => {
+
+    try {
+        const produtoEditar = {
+            id: req.params.id,
+            marca: req.body.marca,
+            modelo: req.body.modelo,
+            ano: req.body.ano,
+            placa: req.body.placa,
+            valor_diaria: req.body.valor_diaria,
+            status: req.body.status
+        }
+
+        const produto = await Produto.update(produtoEditar,{
+        where: { id: req.params.id}
+    })
+
+    const produtoup = await Produto.findByPk(req.params.id)
+    res.status(200).json(produtoup)
+
 }catch(err){
     res.status(500).json(err)
 }
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import express from 'express'
 import { criarProduto } from './controllers/ProdutoController.js'
 import { totalProdutos } from './controllers/ProdutoController.js'
 import { deletarProduto } from './controllers/ProdutoController.js'
+import { editarProduto } from './controllers/ProdutoController.js'
 
 const router = express.Router()
 
 router.post('/cadastro', criarProduto)
 router.get('/todosProdutos', totalProdutos)
 router.delete('/deletar/:id', deletarProduto)
+router.put('/editar/:id', editarProduto)
 
 export default router
